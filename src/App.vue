fdb616faae1e74ee3e2789b649af03d437d1f225
GreyGao <rh87116938@163.com>
2017-09-16 15:05:31 +0800

    add a reset status for resume-editor.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      text: '你好'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+p{ color:red;}
+</style>
